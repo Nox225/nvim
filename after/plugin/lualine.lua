f4aa149e929c86daa2fc9a5cadec5e43639e2a59
Antoś <filip.domanski@arpinformatyka.pl>
2026-08-28 09:44:24 +0200

    lualine better UI

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,16 +1,21 @@
 require('lualine').setup {
   options = {
     theme = 'ayu_dark',
-    icons_enabled = true, -- zostawiamy True dla plikow, ale usuwamy konkretne komponenty ponizej
+    icons_enabled = true,
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     globalstatus = true,
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff' }, -- usunięte: 'diagnostics'
-    lualine_c = { 'filename' },
-    lualine_x = { 'filetype' }, -- usunięte: 'encoding', 'fileformat'
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = {
+      {
+        'filename',
+        color = { fg = '#e6b450', gui = 'bold' },
+      },
+    },
+    lualine_x = {},
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
   },
